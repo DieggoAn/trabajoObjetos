7e0347f46aa0d5d74e3bf1b965e6ae2acfcd8a25
DieggoAn <89940535+DieggoAn@users.noreply.github.com>
2025-10-07 15:39:59 -0300

    Add files via upload
    primera version de base de datos faltan FK

---
+++ empleado.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 07-10-2025 a las 20:39:19
+-- Versión del servidor: 11.6.2-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `gestiondeempleados`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `rutEmpleado` int(12) NOT NULL,
+  `nombres` varchar(25) NOT NULL,
+  `apellidoPaterno` varchar(25) NOT NULL,
+  `apellidoMaterno` varchar(25) NOT NULL,
+  `direccion` varchar(200) NOT NULL,
+  `fechaNacimiento` int(11) NOT NULL,
+  `inicioContrato` int(11) NOT NULL,
+  `salario` decimal(10,2) NOT NULL,
+  `numeroTelefono` varchar(15) NOT NULL,
+  `idProyecto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`rutEmpleado`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
