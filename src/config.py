5db3ea1f11a8672d927e26c82915a43c8b7ff678
hideonn1 <lorcapedro883@gmail.com>
2025-10-11 20:28:47 -0300

    feat: Ingresión de datos de prueba de departamento(s)

--- src/config.py
+++ src/config.py
@@ -1,2 +1,25 @@
-# SQL connection configuration
+# SQL connection configuration y datos de prueba
 
+import mysql.connector
+conexion1 = mysql.connector.connect(host="localhost",user="root",passwd="",database="backend_proyecto")
+
+cursor1 = conexion1.cursor()
+
+# Sentencia SQL
+sql = "INSERT INTO Departamento (id_departamento, nombre) VALUES (%s, %s)"
+
+# Datos de ejemplo
+# Lista de tuplas con varios departamentos
+datos = [
+    ("DEP01", "Recursos Humanos"),
+    ("DEP02", "Finanzas"),
+    ("DEP03", "TI"),
+    ("DEP04", "Marketing")
+]
+
+# Ejecutar todos de una vez
+cursor1.executemany(sql, datos)
+
+# Ejecución
+conexion1.commit()
+conexion1.close()
