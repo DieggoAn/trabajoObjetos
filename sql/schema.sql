d7ad1b1191e23121c52a93d0c813ccaf75f5bcd0
hideonn1 <lorcapedro883@gmail.com>
2025-10-15 13:48:48 -0300

    feat: Cambios mínimos en la base de datos para mayor compatibilidad con las IDs con autoincrementación

--- sql/schema.sql
+++ sql/schema.sql
@@ -10,14 +10,19 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- Schema backend_proyecto
 -- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `backend_proyecto` ;
+
+-- -----------------------------------------------------
+-- Schema backend_proyecto
+-- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `backend_proyecto` DEFAULT CHARACTER SET utf8 ;
 USE `backend_proyecto` ;
 
 -- -----------------------------------------------------
--- Table `Usuario_basico`
+-- Table `backend_proyecto`.`usuario_basico`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Usuario_basico` ;
-CREATE TABLE `Usuario_basico` (
+DROP TABLE IF EXISTS `backend_proyecto`.`usuario_basico` ;
+
+CREATE TABLE IF NOT EXISTS `backend_proyecto`.`usuario_basico` (
   `rut_usuario` VARCHAR(12) NOT NULL,
   `nombres` VARCHAR(45) NOT NULL,
   `apellido_paterno` VARCHAR(45) NOT NULL,
@@ -26,118 +31,120 @@ CREATE TABLE `Usuario_basico` (
   `numero_telefonico` VARCHAR(15) NOT NULL,
   `contraseña` VARCHAR(60) NOT NULL,
   `rol` VARCHAR(45) NOT NULL DEFAULT 'Empleado',
-  PRIMARY KEY (`rut_usuario`)
-) ENGINE = InnoDB;
+  PRIMARY KEY (`rut_usuario`))
+ENGINE = InnoDB;
+
 
 -- -----------------------------------------------------
--- Table `Informe`
+-- Table `backend_proyecto`.`informe`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Informe` ;
-CREATE TABLE `Informe` (
+DROP TABLE IF EXISTS `backend_proyecto`.`informe` ;
+
+CREATE TABLE IF NOT EXISTS `backend_proyecto`.`informe` (
   `descripcion` TEXT NOT NULL,
   `formato` VARCHAR(45) NULL,
   `fecha` DATE NOT NULL,
-  `id_informe` VARCHAR(15) NOT NULL,
+  `id_informe` INT NOT NULL AUTO_INCREMENT,
   `rut_usuario` VARCHAR(12) NOT NULL,
   PRIMARY KEY (`id_informe`),
   INDEX `fk_Informe_Usuario1_idx` (`rut_usuario` ASC),
   CONSTRAINT `fk_Informe_Usuario1`
     FOREIGN KEY (`rut_usuario`)
-    REFERENCES `Usuario_basico` (`rut_usuario`)
+    REFERENCES `backend_proyecto`.`usuario_basico` (`rut_usuario`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-) ENGINE = InnoDB;
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
 
 -- -----------------------------------------------------
--- Table `Departamento`
+-- Table `backend_proyecto`.`departamento`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Departamento` ;
-CREATE TABLE `Departamento` (
-  `id_departamento` VARCHAR(15) NOT NULL,
+DROP TABLE IF EXISTS `backend_proyecto`.`departamento` ;
+
+CREATE TABLE IF NOT EXISTS `backend_proyecto`.`departamento` (
+  `id_departamento` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id_departamento`)
-) ENGINE = InnoDB;
+  `descripcion` VARCHAR(150) NULL,
+  PRIMARY KEY (`id_departamento`))
+ENGINE = InnoDB;
+
 
 -- -----------------------------------------------------
--- Table `Proyecto`
+-- Table `backend_proyecto`.`proyecto`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Proyecto` ;
-CREATE TABLE `Proyecto` (
-  `id_proyecto` VARCHAR(15) NOT NULL,
+DROP TABLE IF EXISTS `backend_proyecto`.`proyecto` ;
+
+CREATE TABLE IF NOT EXISTS `backend_proyecto`.`proyecto` (
+  `id_proyecto` INT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(45) NOT NULL,
-  `descripcion` VARCHAR(150) NOT NULL,
+  `descripcion` TEXT NOT NULL,
   `fecha_inicio` DATE NOT NULL,
-  `id_departamento` VARCHAR(15) NOT NULL,
+  `id_departamento` INT NOT NULL,
   PRIMARY KEY (`id_proyecto`),
   INDEX `fk_Proyecto_Departamento1_idx` (`id_departamento` ASC),
   CONSTRAINT `fk_Proyecto_Departamento1`
     FOREIGN KEY (`id_departamento`)
-    REFERENCES `Departamento` (`id_departamento`)
+    REFERENCES `backend_proyecto`.`departamento` (`id_departamento`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-) ENGINE = InnoDB;
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
 
 -- -----------------------------------------------------
--- Table `RegistroTiempo`
+-- Table `backend_proyecto`.`registro_tiempo`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `RegistroTiempo` ;
-CREATE TABLE `RegistroTiempo` (
-  `id_registro_tiempo` VARCHAR(15) NOT NULL,
+DROP TABLE IF EXISTS `backend_proyecto`.`registro_tiempo` ;
+
+CREATE TABLE IF NOT EXISTS `backend_proyecto`.`registro_tiempo` (
   `fecha` DATE NOT NULL,
   `horas_trabajadas` INT NOT NULL,
   `descripcion_tarea` VARCHAR(150) NOT NULL,
-  `id_departamento` VARCHAR(15) NOT NULL,
+  `id_registro_tiempo` INT NOT NULL AUTO_INCREMENT,
   `rut_usuario` VARCHAR(12) NOT NULL,
-  `id_proyecto` VARCHAR(15) NOT NULL,
+  `id_proyecto` INT NOT NULL,
   PRIMARY KEY (`id_registro_tiempo`),
   INDEX `fk_RegistroTiempo_Usuario1_idx` (`rut_usuario` ASC),
   INDEX `fk_RegistroTiempo_Proyecto1_idx` (`id_proyecto` ASC),
-  INDEX `fk_RegistroTiempo_Departamento1_idx` (`id_departamento` ASC),
   CONSTRAINT `fk_RegistroTiempo_Usuario1`
     FOREIGN KEY (`rut_usuario`)
-    REFERENCES `Usuario_basico` (`rut_usuario`)
+    REFERENCES `backend_proyecto`.`usuario_basico` (`rut_usuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_RegistroTiempo_Proyecto1`
     FOREIGN KEY (`id_proyecto`)
-    REFERENCES `Proyecto` (`id_proyecto`)
+    REFERENCES `backend_proyecto`.`proyecto` (`id_proyecto`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_RegistroTiempo_Departamento1`
-    FOREIGN KEY (`id_departamento`)
-    REFERENCES `Departamento` (`id_departamento`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-) ENGINE = InnoDB;
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
 
 -- -----------------------------------------------------
--- Table `Usuario_Detalle`
+-- Table `backend_proyecto`.`usuario_detalle`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `Usuario_Detalle` ;
-CREATE TABLE `Usuario_Detalle` (
+DROP TABLE IF EXISTS `backend_proyecto`.`usuario_detalle` ;
+
+CREATE TABLE IF NOT EXISTS `backend_proyecto`.`usuario_detalle` (
   `rut_usuario` VARCHAR(12) NOT NULL,
   `direccion` VARCHAR(45) NOT NULL,
   `fecha_inicio_contrato` DATE NOT NULL,
   `salario` INT NOT NULL,
   `rol` VARCHAR(20) NOT NULL,
-  `id_departamento` VARCHAR(15) NOT NULL,
+  `id_departamento` INT NOT NULL,
   PRIMARY KEY (`rut_usuario`),
   INDEX `fk_Usuario_Detalle_Departamento1_idx` (`id_departamento` ASC),
   CONSTRAINT `fk_Usuario_Detalle_Usuario_basico1`
     FOREIGN KEY (`rut_usuario`)
-    REFERENCES `Usuario_basico` (`rut_usuario`)
+    REFERENCES `backend_proyecto`.`usuario_basico` (`rut_usuario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Usuario_Detalle_Departamento1`
     FOREIGN KEY (`id_departamento`)
-    REFERENCES `Departamento` (`id_departamento`)
+    REFERENCES `backend_proyecto`.`departamento` (`id_departamento`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-) ENGINE = InnoDB;
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
--- -----------------------------------------------------
--- Restore settings
--- -----------------------------------------------------
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
