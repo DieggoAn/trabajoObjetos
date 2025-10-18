686edc442d92a15b2e5e46bce95d75dda6c0e155
Juno <juan.navarrete.fritz@gmail.com>
2025-10-17 21:09:32 -0300

    Comienzo de funcion de conversion a pdf

--- src/models/InformeEmpleado.py
+++ src/models/InformeEmpleado.py
@@ -1,5 +1,8 @@
 from Informe import Informe
 from models.interfaces.ExportarInformeInterfaz import ExportarInformeInterfaz
+from fpdf import FPDF
+from datetime import datetime
+from config import conectar_db
 
 class InformeEmpleado(Informe, ExportarInformeInterfaz):
     def __init__ (self,
@@ -20,7 +23,22 @@ class InformeEmpleado(Informe, ExportarInformeInterfaz):
                 f"RUT Empleado: {self.rutEmpleado}")
 
     def formatearDatosParaPDF(self):
-        pass
+        class pdf(FPDF):
+            def header(self):
+                if hasattr(self, 'document_title'):
+                    self.set_font(family = 'Arial', style = 'B', size = 16)
+                    self.cell(w = 0, h = 10, text = self.document_title, border = 0, ln = 1, align = 'C')
+
+            def cuerpo(self):
+                pass
+
+            def footer(self):
+                self.set_y(-15)
+                self.set_font(family = "Arial", style = "I", size = 12)
+                self.cell(w = 0, h = 10, text = f"Generado el {datetime.now().strftime('%d/%m/%y')}", ln = 0, align = "C")
+
+
+        pdf.output(name = f'Informe Empleado{datetime.now().strftime('%d/%m/%y')}', dest='"C:\Users\juann\Downloads"')
 
     def formatearDatosParaExcel(self):
         pass
