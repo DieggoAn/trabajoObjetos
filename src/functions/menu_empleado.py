06aabd800414de075a00f84bf8b31d033aaa53b7
Juno <juan.navarrete.fritz@gmail.com>
2025-11-05 22:21:38 -0300

    algunos cambios

--- src/functions/menu_empleado.py
+++ src/functions/menu_empleado.py
@@ -11,30 +11,26 @@ def menu_empleado(empleado: Empleado):
     print("¡Bienvenido Empleado!")
     while True:
         print("--- OPCIONES DISPONIBLES ---\n")
-        print("OPCIÓN 1. GESTIONAR REGISTROS DE TIEMPO")
-        print("OPCIÓN 2. GESTIONAR INFORMES")
-        print("OPCIÓN 3. GESTIONAR PROYECTOS")
-        print("OPCIÓN 4. CERRAR SESIÓN\n")
+        print("OPCIÓN 1. GESTIONAR INFORMES")
+        print("OPCIÓN 2. GESTIONAR PROYECTOS")
+        print("OPCIÓN 3. CERRAR SESIÓN\n")
 
         try: 
-            opcion_user = int(input("Ingresar opción (1 - 4): "))
+            opcion_user = int(input("Ingresar opción (1 - 3): "))
         except ValueError:
             print("Debe ingresar una opción válida para continuar.")
 
-        if opcion_user not in (1,2,3,4):
-            print("Debe ingresar una de las opciones disponibles (1 - 4) para continuar.")
+        if opcion_user not in (1,2,3):
+            print("Debe ingresar una de las opciones disponibles (1 - 3) para continuar.")
             continue
 
         match opcion_user:
             case 1:
-                menu_gestion_registrotiempo(empleado)
-
-            case 2:
                 menu_gestion_informe(empleado)
 
-            case 3:
+            case 2:
                 menu_gestion_proyecto(empleado)
 
-            case 4:
+            case 3:
                 print("Sesión cerrada exitosamente.")
                 break
